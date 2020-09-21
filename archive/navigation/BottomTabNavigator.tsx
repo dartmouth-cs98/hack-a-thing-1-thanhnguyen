e53b0408825f69c350c1a205678e6c109e52fdde
Thanh Nguyen Jr <tcnguyen483@gmail.com>
2020-09-21 11:45:51 -0400

    restart expo project to try and fix app render problem

--- archive/navigation/BottomTabNavigator.tsx
+++ archive/navigation/BottomTabNavigator.tsx
@@ -1,17 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
 
+
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
-export default function BottomTabNavigator() {
+export default function BottomTabNavigator(): JSX.Element {
   const colorScheme = useColorScheme();
 
   return (
